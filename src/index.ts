4e61628a80878b374762c4e641b8ec418fdaec7b
Revaldi <revaldii32@gmail.com>
2023-04-05 20:14:05 +0700

    [BFIX] - Fixing ESLint check

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express from 'express'
+import type { Application, Request, Response, NextFunction } from 'express'
 
 const app: Application = express()
-const port: Number = 4000
+const port = 4000
 
 // Routing
 app.use('/help', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: '200' })
+  res.status(200).send({ status: '200' })
 })
 
 // Init app
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
