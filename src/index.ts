a40ca8e6c0a27a1c54fd65545c91bd422b84e139
Revaldi <revaldii32@gmail.com>
2023-04-10 11:34:16 +0700

    [IMP] - Modify routes, add validation and logger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import express, { type Application, type Request, type Response, type NextFunction } from 'express'
+import express, { type Application } from 'express'
+import { routes } from './routes'
+import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = 4000
 
-// Routing
-app.use('/help', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: '200' })
+// Parse request body
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// CORS access handler
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
 })
 
+// Routing
+routes(app)
+
 // Init app
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => logger.info(`Server is listening on port ${port}`))
